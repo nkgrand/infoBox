c968532f21dee493f4d1027b66afb3910241d053
Andrey <nkgrand25@gmail.com>
2020-12-04 02:21:06 +0200

    added state added props added fetch added server adress

--- src/infoBox.jsx
+++ src/infoBox.jsx
@@ -2,74 +2,35 @@ import React from "react";
 import styles from "./infobox.module.scss";
 import { Slide } from "./slide.jsx";
 
-const slides = [
-  {
-    title: "Time to Share: 6 for $3.99*",
-    img:
-      "https://res.cloudinary.com/dx4wkpab8/image/upload/v1573640170/comp_plate_promo1_wsmolg.png",
-    description:
-      "Lorem ipsum dolor sit amet. consectetur adipisicing elit, sed do eiusmod tempor incididunt ut la bore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exefcitalion ullamoo laboris nisi ut aliquip ex ea commodo oonsequat.",
-    note:
-      "* At vero eos et accusamus et iusto odo dtgntsslmos duclmus qui blandltlis praesentlum voluptatum delenrtl atque corruptl quos doQres et quas molestlas exceptun sint occaecatl cupidrtate non pro v dent, slmllique sunt In culpa qui otflcia deserunt mollrtia anlmi. id est la bo aim et dolorum tuga.",
-    productUrl:
-      "https://res.cloudinary.com/dx4wkpab8/image/upload/v1573640170/comp_plate_promo1_wsmolg.png",
-    id: 1,
-  },
-  {
-    title: "Rise 'n shine",
-    img:
-      "https://res.cloudinary.com/dx4wkpab8/image/upload/v1573640171/comp_plate_promo2_nlqjfe.png",
-    description:
-      "Lorem ipsum dolor sit amet. consectetur adipisicing elit, sed do eiusmod tempor incididunt ut la bore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exefcitalion ullamoo laboris nisi ut aliquip ex ea commodo oonsequat.",
-    note:
-      "* At vero eos et accusamus et iusto odo dtgntsslmos duclmus qui blandltlis praesentlum voluptatum delenrtl atque corruptl quos doQres et quas molestlas exceptun sint occaecatl cupidrtate non pro v dent, slmllique sunt In culpa qui otflcia deserunt mollrtia anlmi. id est la bo aim et dolorum tuga.",
-    productUrl:
-      "https://res.cloudinary.com/dx4wkpab8/image/upload/v1573640171/comp_plate_promo2_nlqjfe.png",
-    id: 2,
-  },
-  {
-    title: "PM Snackers: Brownie Bites",
-    img:
-      "https://res.cloudinary.com/dx4wkpab8/image/upload/v1573640171/comp_plate_promo4_f87x7g.png",
-    description:
-      "Lorem ipsum dolor sit amet. consectetur adipisicing elit, sed do eiusmod tempor incididunt ut la bore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exefcitalion ullamoo laboris nisi ut aliquip ex ea commodo oonsequat.",
-    note:
-      "* At vero eos et accusamus et iusto odo dtgntsslmos duclmus qui blandltlis praesentlum voluptatum delenrtl atque corruptl quos doQres et quas molestlas exceptun sint occaecatl cupidrtate non pro v dent, slmllique sunt In culpa qui otflcia deserunt mollrtia anlmi. id est la bo aim et dolorum tuga.",
-    productUrl:
-      "https://res.cloudinary.com/dx4wkpab8/image/upload/v1573640171/comp_plate_promo4_f87x7g.png",
-    id: 3,
-  },
-  {
-    title: "PM Snackers: Brownie Bites 2",
-    img:
-      "https://res.cloudinary.com/dx4wkpab8/image/upload/v1573640171/comp_plate_promo3_wnp43x.png",
-    description:
-      "Lorem ipsum dolor sit amet. consectetur adipisicing elit, sed do eiusmod tempor incididunt ut la bore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exefcitalion ullamoo laboris nisi ut aliquip ex ea commodo oonsequat.",
-    note:
-      "* At vero eos et accusamus et iusto odo dtgntsslmos duclmus qui blandltlis praesentlum voluptatum delenrtl atque corruptl quos doQres et quas molestlas exceptun sint occaecatl cupidrtate non pro v dent, slmllique sunt In culpa qui otflcia deserunt mollrtia anlmi. id est la bo aim et dolorum tuga.",
-    productUrl:
-      "https://res.cloudinary.com/dx4wkpab8/image/upload/v1573640171/comp_plate_promo4_f87x7g.png",
-    id: 4,
-  },
-];
-
 export class InfoBox extends React.Component {
   state = {
     activeIndex: 0,
+    slides: [],
   };
 
+  async componentDidMount() {
+    const response = await fetch(
+      "https://my-json-server.typicode.com/nkgrand/ads-box-server/advertisments"
+    );
+    const slides = await response.json();
+    this.setState({ slides });
+  }
+
   renderSlide() {
-    return slides.map((slide, i) => {
+    return this.state.slides.map((slide, i) => {
       const key = Math.floor(Math.random() * 1000).toString();
       const isActive = this.state.activeIndex === i;
-      return <Slide {...{ key, isActive, ...slide }} />;
+      const onButtonClick = (data) => {
+        console.log(data);
+      };
+      return <Slide {...{ key, isActive, ...slide, onButtonClick }} />;
     });
   }
 
   increeseIndex() {
     this.setState({
       activeIndex:
-        this.state.activeIndex + 1 < slides.length
+        this.state.activeIndex + 1 < this.state.slides.length
           ? this.state.activeIndex + 1
           : 0,
     });
@@ -80,12 +41,13 @@ export class InfoBox extends React.Component {
       activeIndex:
         this.state.activeIndex >= 1
           ? this.state.activeIndex - 1
-          : slides.length - 1,
+          : this.state.slides.length - 1,
     });
   }
 
   render() {
     const slidesJsx = this.renderSlide();
+    console.log("render");
     return (
       <div className={styles.infoBox}>
         <div className={styles.slides}>{slidesJsx}</div>
--- src/slide.jsx
+++ src/slide.jsx
@@ -3,12 +3,13 @@ import styles from "./slide.module.scss";
 
 export class Slide extends React.Component {
   render() {
-    const { isActive, img, title, description } = this.props;
+    const { isActive, img, title, description, onButtonClick } = this.props;
     return (
       <div className={`${styles.slide} ${isActive ? styles.active : ""}`}>
         <img className={styles.img} src={img} alt="" />
         <h2>{title}</h2>
         <p>{description}</p>
+        <button onClick={() => onButtonClick("Hello" + title)}>Click Me</button>
       </div>
     );
   }
